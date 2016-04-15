33ea604b726b0000148b0712ef7675bf47cbf39f
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2016-04-15 15:17:19 -0400

    Added a prompt

--- mortgage_calculator.rb
+++ mortgage_calculator.rb
@@ -1,1 +1,20 @@
-mortgage_calculator.rb
+
+def prompt(message)
+	Kernel.puts("=>#{message}")
+end
+
+prompt(" Welcome to your own Mortgage Calculator. Enter your name.")
+
+name = ''
+	loop do 
+		name = Kernel.gets().chomp()
+
+		if name.empty?()
+			prompt("Enter a valid name.")
+		else
+			break
+		end
+		
+	end
+
+	prompt('Hi #{name}')
