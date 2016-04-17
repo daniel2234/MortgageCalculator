744aefa9e136e6e058f15a82fbd5bd9447c0b05a
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2016-04-17 12:00:44 -0400

    added proper prompts

--- mortgage_calculator.rb
+++ mortgage_calculator.rb
@@ -1,20 +1,49 @@
+require 'pry'
 
 def prompt(message)
-	Kernel.puts("=>#{message}")
+	Kernel.puts("=> #{message}")
 end
+
 
 prompt(" Welcome to your own Mortgage Calculator. Enter your name.")
 
 name = ''
-	loop do 
-		name = Kernel.gets().chomp()
+loop do
+  name = Kernel.gets().chomp()
+  if name.empty?()
+    prompt("Make sure you use a valid name")
+  else
+    break
+  end
+end
 
-		if name.empty?()
-			prompt("Enter a valid name.")
-		else
-			break
-		end
-		
+prompt("Hello #{name}. We will calculate your loan duration in months based on your monthly rate.")
+
+#main loop
+loop do
+	loan_Amount = ''
+	loop do
+		prompt("What is your monthly loan amount?")
+		loan_Amount = Kernel.gets().chomp()
+		break
 	end
 
-	prompt('Hi #{name}')
+	interest = ''
+	loop do
+		prompt("What is your interest rate?")
+		interest = Kernel.gets().chomp()
+		break
+	end
+
+	loan_Duration = ''
+	loop do
+		prompt("What is your loan duration (months)?")
+		loan_Duration = Kernel.gets().chomp()
+		break
+	end
+	prompt("Do you want to perform another calculation? (Y to calculate again) ")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you #{name} for using the Mortgage Calculator!")
